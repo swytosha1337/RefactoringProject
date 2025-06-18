85b1ce87d65287047c96abeb607d2b4f2eba37e3
Lyubov <lomovala@gmail.com>
2025-06-19 01:06:26 +0300

    Project for refactoring

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppWithoutRefactoring
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            List <string> item = new List<string>() {"Клавиатура"};
+            OrderCreater newOrder = new OrderCreater("Иванов И.И.", item, "По карте");
+        }
+    }
+}
---
+++ OrderCreater.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppWithoutRefactoring
+{
+    internal class OrderCreater
+    {
+        public OrderCreater(string customerName, List<string> items, string paymentMethod) 
+        {
+            // Валидация
+            if (string.IsNullOrEmpty(customerName))
+            {
+                throw new ArgumentException("Данные заказчика указаны не корректно!");
+            }
+            if (items == null || items.Count == 0)
+            {
+                throw new ArgumentException("Не выбраны позиции для заказа!");
+            }
+            
+            // Логика обработки заказа
+            decimal total = 0;
+            foreach (var item in items)
+            {
+                if (item == "Ноутбук") 
+                    total = total + 1200;
+                else if (item == "Мышь") 
+                    total = total +25;
+                else if (item == "Клавиатура") 
+                    total = total + 50;
+                else if (item == "Камера")
+                    total = total + 500;
+                else if (item == "Колонки")
+                    total = total + 150;
+            }
+           
+            // Скидки
+            if (items.Count > 2)
+            {
+                total *= (decimal)0.9; // 10% скидка
+            }
+            
+            // Обработка платежа
+            if (paymentMethod == "По карте")
+            {
+                MessageBox.Show($"Обработка платежа по кредитной карте: {total}");
+            }
+            else if (paymentMethod == "PayPal")
+            {
+                Console.WriteLine($"Обработка PayPal платежа  {total}");
+            }
+            else
+            {
+                throw new ArgumentException("Неподдерживаемый способ оплаты.");
+            }
+
+            // Логирование
+            string logEntry = $"Order processed for {customerName} at {DateTime.Now}";
+            File.WriteAllText(@".\logs\order.txt", logEntry);
+
+            // Отправка уведомления
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            MailAddress from = new MailAddress("lomovala@gmail.com", "LomovaLA");
+            MailAddress to = new MailAddress("lomovala@gmail.com");
+            var message = new MailMessage(from, to)
+            {
+                Subject = "Новый заказ",
+                Body = $"<h2>Новый заказ для {customerName} общей стоимостью {total:C}.</h2>"
+            };
+            smtpClient.Send(message);
+        }
+    }
+}
